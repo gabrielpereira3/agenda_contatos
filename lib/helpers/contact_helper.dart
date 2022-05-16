635db38626fc7edbb56c72482a2643fab8503dc2
Gabriel <gabriel.soares14563@gmail.com>
2022-05-16 11:30:24 -0300

    Home page criada para testes iniciais com o banco de dados.

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -30,7 +30,7 @@ class ContactHelper {
 
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
 
     return openDatabase(
         path, version: 1, onCreate: (Database db, int newerVersion) async {
@@ -96,6 +96,8 @@ class Contact {
   String? email;
   String? phone;
   String? img;
+
+  Contact();
 
   Contact.fromMap(Map map){
     id = map[idColumn];
